bde4cd840eabffa6e270e5f4b29679d363683169
Mirb88 <51285621+Mirb88@users.noreply.github.com>
2026-08-27 22:06:58 +0200

    Web3 3 Core 8
    Web3 Core import 8

---
+++ robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.mirb.investments/sitemap.xml',
+  };
+}
