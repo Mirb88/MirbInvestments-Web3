50b5e1244d367f450e8671972ffbf0912c978508
Mirb88 <51285621+Mirb88@users.noreply.github.com>
2026-09-05 22:39:17 +0200

    Add db property to authentication hook

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -3,6 +3,7 @@ export function useAuth() {
     user: null,
     isAuthenticated: false,
     isLoading: false,
+    db: null, //
     updateUserPassword: async (currentPassword: string, newPassword: string) => {
       throw new Error("Funkcionalnost promjene lozinke trenutno nije omogućena u demo režimu.");
     },
